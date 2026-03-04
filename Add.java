2df4c909c8a61ba44f5112ad0904de7c7f8169ac
Chaitra371 <chaitra0920@gmail.com>
2026-03-04 11:12:53 +0530

    Include third number in sum calculation

--- Add.java
+++ Add.java
@@ -2,9 +2,10 @@ class Add {
     public static void main(String[] args) {
         int firstNumber = 10;
         int secondNumber = 20;
+        int thirdNumber = 30;
         
         
-        int sum = firstNumber + secondNumber; 
+        int sum = firstNumber + secondNumber + thirdNumber; 
         
         
         System.out.println( sum);
