f14eb87b011caac4bec4ba0c5ec6962672feb4d9
Chaitra371 <chaitra0920@gmail.com>
2026-03-04 11:09:23 +0530

    Add Java program to sum two numbers

---
+++ Add.java
@@ -0,0 +1,12 @@
+class Add {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 20;
+        
+        
+        int sum = firstNumber + secondNumber; 
+        
+        
+        System.out.println(firstNumber + " + " + secondNumber + " = " + sum);
+    }
+}
